9d6524ec4472ec053961535f6ea24dd8c6669990
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2024-07-25 08:48:56 +0900

    first commit

---
+++ Ramanujan_pi.c
@@ -0,0 +1,123 @@
+#define REPEAT_NUM 7000
+#define FLOAT_BIT 35000
+
+#include <stdio.h>
+#include <gmp.h>
+#include <math.h>
+#include <omp.h>
+
+void factorial(mpz_t result, unsigned long int n) {
+    mpz_set_ui(result, 1);
+    #pragma omp parallel for
+    for (unsigned long int i = 1; i <= n; i++) {
+        mpz_mul_ui(result, result, i);
+    }
+}
+
+void power_ui_ui(mpz_t result, unsigned long int base, unsigned long int exponent){
+    mpz_set_ui(result, 1);
+    #pragma omp parallel for
+    for(unsigned long int i=0; i<exponent; i++){
+        mpz_mul_ui(result, result, base);
+    }
+}
+
+void power_mpz_ui(mpz_t result, mpz_t base, unsigned long int exponent){
+    mpz_t tmp;
+    mpz_init2(tmp, FLOAT_BIT);
+    mpz_set_ui(tmp, 1);
+    #pragma omp parallel for
+    for(unsigned long int i=0; i<exponent; i++){
+        mpz_mul(tmp, tmp, base);
+    }
+    mpz_swap(tmp, result);
+    mpz_clear(tmp);
+}
+
+int main() {
+    mpf_t pi_inverse;
+    mpf_init2(pi_inverse, FLOAT_BIT);
+    mpf_set_ui(pi_inverse, 0);
+
+    #pragma omp parallel
+    {
+        mpz_t _4n_fact, _4pown, _99pown, n_fact, block3, tmp;
+        mpf_t tmp_f, block3_f;
+        mpz_init2(_4n_fact, FLOAT_BIT);
+        mpz_init2(_4pown, FLOAT_BIT);
+        mpz_init2(_99pown, FLOAT_BIT);
+        mpz_init2(n_fact, FLOAT_BIT);
+        mpz_init2(block3, FLOAT_BIT);
+        mpz_init2(tmp, FLOAT_BIT);
+        mpf_init2(tmp_f, FLOAT_BIT);
+        mpf_init2(block3_f, FLOAT_BIT);
+
+        #pragma omp for schedule(static)
+        for (unsigned long int n = 0; n < REPEAT_NUM; n++) {
+            unsigned long int _4n = 4 * n;
+            factorial(_4n_fact, _4n);
+
+            unsigned long int block2 = 1103 + (26390 * n);
+
+            power_ui_ui(_4pown, 4, n);
+            power_ui_ui(_99pown, 99, n);
+            factorial(n_fact, n);
+            mpz_mul(block3, _4pown, _99pown);
+            mpz_mul(block3, block3, n_fact);
+            power_mpz_ui(block3, block3, 4);
+
+            mpz_mul_ui(tmp, _4n_fact, block2);
+            mpf_set_z(tmp_f, tmp);
+            mpf_set_z(block3_f, block3);
+            mpf_div(tmp_f, tmp_f, block3_f);
+            mpf_add(pi_inverse, pi_inverse, tmp_f);
+        }
+
+        mpz_clear(_4n_fact);
+        mpz_clear(_4pown);
+        mpz_clear(_99pown);
+        mpz_clear(n_fact);
+        mpz_clear(block3);
+        mpz_clear(tmp);
+        mpf_clear(tmp_f);
+        mpf_clear(block3_f);
+    }
+
+    mpf_t sqrt2, _2sqrt2;
+    mpf_init2(sqrt2, FLOAT_BIT);
+    mpf_init2(_2sqrt2, FLOAT_BIT);
+    mpf_sqrt_ui(sqrt2, 2);
+    mpf_mul_ui(_2sqrt2, sqrt2, 2);
+    mpf_clear(sqrt2);
+
+    mpz_t _99, _99squared;
+    mpf_t _99squared_f;
+    mpz_init_set_ui(_99, 99);
+    mpz_init(_99squared);
+    mpf_init(_99squared_f);
+    power_mpz_ui(_99squared, _99, 2);
+    mpf_set_z(_99squared_f, _99squared);
+    mpz_clear(_99);
+    mpz_clear(_99squared);
+
+    mpf_t block1;
+    mpf_init2(block1, FLOAT_BIT);
+    mpf_div(block1, _2sqrt2, _99squared_f);
+    mpf_clear(_2sqrt2);
+    mpf_clear(_99squared_f);
+
+    mpf_mul(pi_inverse, block1, pi_inverse);
+    mpf_clear(block1);
+
+    mpf_t pi, _1;
+    mpf_init2(pi, FLOAT_BIT);
+    mpf_init_set_ui(_1, 1);
+    mpf_div(pi, _1, pi_inverse);
+    mpf_clear(pi_inverse);
+    mpf_clear(_1);
+
+    gmp_printf("%.100000Ff\n", pi);
+    mpf_clear(pi);
+
+    return 0;
+}
---
+++ acuracy_num.c
@@ -0,0 +1,72 @@
+// gcc acuracy_num.c -o acuracy_num -lgmp -fopenmp && ./acuracy_num
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+
+#define FLOAT_BIT 100000
+#define N 100001 // 読み込み桁数
+
+char fname_calc[] = "calc_pi.txt";
+char fname_correct[] = "correct_pi.txt";
+
+int main(){
+    FILE *fp_calc,*fp_correct; // FILE型構造体
+    
+	char str_calc[N];
+	char str_correct[N];
+
+    // ファイルを開く。失敗するとNULLを返す。
+	fp_calc = fopen(fname_calc, "r");
+	fp_correct = fopen(fname_correct, "r");
+
+	if(fp_calc == NULL) {
+		printf("%s file not open!\n", fname_calc);
+		return -1;
+	}
+
+    if(fp_correct == NULL) {
+		printf("%s file not open!\n", fname_correct);
+		return -1;
+	}
+    
+	fgets(str_calc, N, fp_calc);
+    fgets(str_correct, N, fp_correct);
+
+    mpf_t correct_num, calc_num, diff;
+    mpf_init2(correct_num, FLOAT_BIT);  //正確な値
+    mpf_init2(calc_num, FLOAT_BIT);     //計算値
+    mpf_init2(diff, FLOAT_BIT);         //計算誤差
+
+    mpf_set_str(calc_num,str_calc,10);
+    mpf_set_str(correct_num,str_correct,10);
+
+    if(mpf_cmp(correct_num, calc_num) > 0)  //correct_num > calc_num
+        mpf_sub(diff, correct_num, calc_num);
+    else
+        mpf_sub(diff, calc_num, correct_num);
+
+    // gmp_printf("diff: %.100000Ff\n", diff);
+
+    int count_digit = 0;
+    while(1){
+        if(mpf_cmp_ui(diff, 0) == 0){
+            count_digit = N;
+            break;
+        }
+        if(mpf_cmp_ui(diff, 1) > 0) break;
+        mpf_mul_ui(diff, diff, 10);
+        count_digit++;
+    }
+
+    printf("精度: 小数点%d桁\n", count_digit-1);
+    
+    mpf_clear(correct_num);
+    mpf_clear(calc_num);
+    mpf_clear(diff);
+
+    // ファイルを閉じる
+	fclose(fp_calc); 
+    fclose(fp_correct);
+ 
+	return 0;
+}
